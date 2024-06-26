5fbfd9b265021efed6b40362b27e6894955bbe07
mrcryptsie <richardkeke15@gmail.com>
2024-06-26 14:35:25 +0100

    Nouvelle modif

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description='Un simple package pour les calculs matriciels',
     author='TITO Lucien',
     author_email='titovlucien@gmail.com',
-    url='https://github.com/votre_compte/matrice_ifri',  # Remplacez par l'URL de votre projet
+    url='https://github.com/mrcryptsie/matrice_ifri', 
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
